45fd4773ec1eaf142ea79ebd648634be225fd686
Matt Oestreich <matthewpoestreich@gmail.com>
2020-09-16 00:41:45 -0500

    update

--- main.go
+++ main.go
@@ -11,56 +11,71 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/oze4/godaddygo"
+	"github.com/oze4/godaddygo/pkg/endpoints"
 )
 
-// GoDaddy holds godaddy api creds
-type GoDaddy struct {
-	key            string
-	secret         string
-	domain         string
-	baselineRecord string
-}
+const icanhazipURL = "https://icanhazip.com"
 
 func main() {
 	godotenv.Load()
 
-	fromapi, err := getFromAPI()
+	api := newIcanhazip()
+
+	apires, err := api.get()
 	if err != nil {
-		log.Fatalf("Error getting public IP from https://icanhazip.com %s\n", err.Error())
+		log.Fatalf("Error getting public IP from %s %s\n", icanhazipURL, err.Error())
 		os.Exit(1)
 	}
 
-	gd := GoDaddy{
-		key:            os.Getenv("GODADDY_APIKEY"),
-		secret:         os.Getenv("GODADDY_APISECRET"),
-		domain:         os.Getenv("GODADDY_DOMAIN"),
-		baselineRecord: os.Getenv("BASELINE_RECORD"),
-	}
+	k := os.Getenv("GODADDY_APIKEY")
+	s := os.Getenv("GODADDY_APISECRET")
+	d := os.Getenv("GODADDY_DOMAIN")
 
-	fromgodaddy, err := getFromGoDaddy(gd)
+	gd := newGoDaddy(k, s, d)
+
+	gdres, err := gd.get()
 	if err != nil {
-		log.Fatalf("Error getting IP from GoDaddy %s\n", err.Error())
-		os.Exit(1)
+		explainStrExit("Error getting IP from GoDaddy: "+err.Error(), 1)
 	}
 
-	tfromapi := strings.TrimSpace(fromapi)
-	tfromgodaddy := strings.TrimSpace(fromgodaddy)
-
-	if tfromapi != tfromgodaddy {
-		fmt.Println("Public IP has changed, updating GoDaddy now. Old: " + tfromgodaddy + " New: " + tfromapi)
-		if err := updateGoDaddy(gd, tfromapi); err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-	} else {
-		fmt.Println("Public IP has not changed: " + tfromapi)
+	if apires == gdres {
+		explainStrExit("Public IP has not changed: "+apires, 0)
 	}
 
-	os.Exit(0)
+	explainStr("Public IP has changed, updating GoDaddy now. Old: " + gdres + " New: " + apires)
+
+	if err := gd.update(apires); err != nil {
+		explainStrExit("Error updating GoDaddy DNS: "+err.Error(), 1)
+	}
+
+	explainStrExit("\nDone\n", 0)
 }
 
-func getFromAPI() (string, error) {
-	resp, err := http.Get("https://icanhazip.com")
+/**
+ * helper functions
+ */
+
+func explainStr(message string) {
+	fmt.Printf("%s\n", message)
+}
+
+func explainStrExit(message string, exitCode int) {
+	fmt.Printf("%s\n", message)
+	os.Exit(exitCode)
+}
+
+/**
+ * icanhazip
+ */
+
+func newIcanhazip() icanhazip {
+	return icanhazip{}
+}
+
+type icanhazip struct{}
+
+func (i icanhazip) get() (string, error) {
+	resp, err := http.Get(icanhazipURL)
 	if err != nil {
 		return "", err
 	}
@@ -72,22 +87,38 @@ func getFromAPI() (string, error) {
 	return strings.TrimSpace(string(body)), nil
 }
 
-func getFromGoDaddy(g GoDaddy) (string, error) {
-	r, e := godaddygo.ConnectProduction(g.key, g.secret).V1().Domain(g.domain).Records().GetByTypeName("A", g.baselineRecord)
+/**
+ * godaddy
+ */
+
+func newGoDaddy(k, s, d string) goDaddy {
+	baseline := os.Getenv("BASELINE_RECORD")
+	api := godaddygo.ConnectProduction(k, s).V1().Domain(d).Records()
+	return goDaddy{baseline, api}
+}
+
+type goDaddy struct {
+	baseline string // BASELINE_RECORD
+	recs     endpoints.Records
+}
+
+// get returns the *value* of your `BASELINE_RECORD`
+func (g *goDaddy) get() (string, error) {
+	r, e := g.recs.GetByTypeName("A", g.baseline)
 	if e != nil {
 		return "", e
 	}
-	return (*r)[0].Data, nil
+	return strings.TrimSpace((*r)[0].Data), nil
 }
 
-func updateGoDaddy(g GoDaddy, newIP string) error {
-	recs := godaddygo.ConnectProduction(g.key, g.secret).V1().Domain(g.domain).Records()
-	r, e := recs.GetByType("A")
+// update sets all A records to your new public IP
+func (g goDaddy) update(newIP string) error {
+	r, e := g.recs.GetByType("A")
 	if e != nil {
 		return e
 	}
 	for _, d := range *r {
-		if e := recs.SetValue(d.Type, d.Name, newIP); e != nil {
+		if e := g.recs.SetValue(d.Type, d.Name, newIP); e != nil {
 			return errors.New("Error setting record: " + d.Name)
 		}
 	}
